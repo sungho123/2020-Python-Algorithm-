7d7000003b5e5345fec1d688be0fcf2a2f5e4567
atwel00 <64391300+atwel00@users.noreply.github.com>
2020-04-29 14:29:14 +0900

    Update 4월4주차 연습문제 1 홍성훈.py

--- 제출/Algorithm/1_04_4주차/4월4주차 연습문제 1 홍성훈.py
+++ 제출/Algorithm/1_04_4주차/4월4주차 연습문제 1 홍성훈.py
@@ -4,15 +4,21 @@ Spyder Editor
 
 This is a temporary script file.
 """
-n = 1
-n1 = 472
-n2 = 385
-sum = 0
+n = 1 #Count 변수
+n1 = 472 #첫번째 자연수
+n2 = 385 #두번째 자연수
+sum = 0 #합계 변수
 
-while n < len(str(n2))+1:
-    tmp = int(n1*(n2%10**n - n2%10**(n-1))/10**(n-1))
+
+while n <= len(str(n2)):# 두번째 자연수의 자리수 만큼 반복
+    #첫번째 자연수 * 각 자리수
+    #10의 n제곱으로 각 자리의 숫자를 계산하여 첫번째 자연수에 곱함
+    tmp = int(n1*(n2%10**n - n2%10**(n-1))/10**(n-1)) #임시 변수
+    #중간 계산된 숫자 출력
     print(tmp) 
+    #자리수 위치 맞게 10의 n제곱을 곱하여 합산
     sum += tmp * 10**(n-1)
     n+=1
 
+#최종 합산 숫자 출력
 print(sum)
