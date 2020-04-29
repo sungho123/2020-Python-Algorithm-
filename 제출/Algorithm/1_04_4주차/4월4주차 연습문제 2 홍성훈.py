573e958940fba4dace599cafa50ee39f6434fb59
atwel00 <64391300+atwel00@users.noreply.github.com>
2020-04-29 16:55:36 +0900

    Update 4월4주차 연습문제 2 홍성훈.py

--- 제출/Algorithm/1_04_4주차/4월4주차 연습문제 2 홍성훈.py
+++ 제출/Algorithm/1_04_4주차/4월4주차 연습문제 2 홍성훈.py
@@ -6,17 +6,21 @@ sum1 = 0 #합계변수
 n = 5 # 인원수 변수
 while i <= n : #인원수 만큼 반복
     print(str(i),"번째 학생의 점수를 입력하세요 : ")
-    tmp = int(input()) #점수 입력 
-    #조건에 따라 입력된 점수 유효성 검증하여 유효한 점수만 저장
-    if tmp > 100 :        
-        print("100보다 클 수 없습니다.")
-    elif tmp < 0 :        
-        print("0보다 작을 수 없습니다.")
-    elif (tmp%5) != 0:
-        print("5점단위로 입력하세요")
-    else :
-        a.append(tmp) #입력된 점수를 리스트변수에 저장
-        i+=1
+
+    try :
+        tmp = int(input()) #점수 입력 
+        #조건에 따라 입력된 점수 유효성 검증하여 유효한 점수만 저장
+        if tmp > 100 :        
+            print("100보다 클 수 없습니다.")
+        elif tmp < 0 :        
+            print("0보다 작을 수 없습니다.")
+        elif (tmp%5) != 0:
+            print("5점단위로 입력하세요")
+        else :
+            a.append(tmp) #입력된 점수를 리스트변수에 저장
+            i+=1
+    except :
+        print("숫자만 입력하세요")
  
 
 for a1 in a: #리스트 변수 반복
