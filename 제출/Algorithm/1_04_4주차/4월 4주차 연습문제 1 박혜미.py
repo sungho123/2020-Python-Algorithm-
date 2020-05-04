93dbcf724482c8f98a168eb7e61d4ddfc5edca1b
hyemi park <hmks012@gmail.com>
2020-05-04 18:55:28 +0900

    Update 4월 4주차 연습문제 1 박혜미.py

--- 제출/Algorithm/1_04_4주차/4월 4주차 연습문제 1 박혜미.py
+++ 제출/Algorithm/1_04_4주차/4월 4주차 연습문제 1 박혜미.py
@@ -1,2 +1,15 @@
 a = int(input())
 b = int(input())
+
+nnn = str(b)[::-1]
+ind = 1
+mul = 0
+sum = 0
+for n in nnn :
+    mul = int(a) * int(n)
+    print( mul )
+    mul *= ind
+    ind *= 10
+    sum += int(mul)
+
+print(sum)
