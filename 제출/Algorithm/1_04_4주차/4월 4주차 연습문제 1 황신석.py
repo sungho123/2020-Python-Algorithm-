c48dd7429d7cac27126e5262d275256b6a2fa8df
ddobagi84 <64541236+ddobagi84@users.noreply.github.com>
2020-05-06 21:18:19 +0900

    Create 4월 4주차 연습문제 1 황신석.py

---
+++ 제출/Algorithm/1_04_4주차/4월 4주차 연습문제 1 황신석.py
@@ -0,0 +1,15 @@
+a = int(input())
+b = int(input())
+
+s1 = str(a)
+s2 = str(b)
+
+step1 = a *  int(s2[2])
+step2 = a *  int(s2[1])
+step3 = a *  int(s2[0])
+result = a * b
+
+print(step1)
+print(step2)
+print(step3)
+print(result)
